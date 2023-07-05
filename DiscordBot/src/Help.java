8263a370be671f2ba94cc352bdf8a3f0eed3356a
Jennifer Spicer <xJenniferSpicer@gmail.com>
2023-07-05 13:58:55 -0400

    Add clear command info

--- DiscordBot/src/Help.java
+++ DiscordBot/src/Help.java
@@ -10,38 +10,18 @@ public class Help extends ModuleBase {
 	@Override
 	public void process() {
 		//queue needed to respond to commands
-		
-//		channel.sendMessage(
-//		"List of available commands:"
-//		+ cmdString("!help", "brings up list of commands.")
-//		+ cmdString("!ping", "prints user's ping.")
-//		+ cmdString("!sage", "returns user's server age. ~Can only use with self!")
-//		+ cmdString("!age", "returns user's Discord age. ~Can only use with self!")
-//		+ cmdString("!choose <option 1> || <option 2>", "chooses a choice between two or more options.")
-//		).queue();
-		
-		//channel.sendMessage("```Java\n TEXT HERE \n```").queue();//make methods for different languages in ModuleBase
-		/*send("!help", "prints list of commands.");
-		send("!ping", "prints user's ping.");
-		send("!sage", "returns user's server age. ** Currently cannot use with users other than self **");
-		send("!age", "returns user's Discord age. ** Currently cannot use with users other than self **");
-		send("!choose <option 1> & <option 2>", "");*/
-		
+
 		//################// EMBED BUILDER //################//
-		
 		EmbedBuilder embedBuilder = new EmbedBuilder();
-		// *** for bold italic
-		embedBuilder.setTitle("***Essentials Commands***");
+		embedBuilder.setTitle("***Essentials Commands***");// *** for bold italic
+		//cmdString SYNTAX:	cmdString("!<command name>", "<command description>.")
 		embedBuilder.setDescription(cmdString("!help", "brings up list of commands.")
-									+ cmdString("!ping", "prints user's ping.")/*
-									//ADD: ability to use !age and !sage on someone else
-									+ cmdString("!sage", "returns user's server age. ~Can only use with self for now!")
-									+ cmdString("!age", "returns user's Discord age. ~Can only use with self for now!")*/
-									+ cmdString("!choose <option 1> || <option 2>", "chooses a choice between two or more options."));
+									+ cmdString("!ping", "prints user's ping.")
+									+ cmdString("!choose <option 1> || <option 2>", "chooses a choice between two or more options.")
+									+ cmdString("!clear <number between 1-100>", "clears messages in the current channel. Cannot clear a.) more than 100 messages at a time and b.) messages older than two weeks old.") );
 		embedBuilder.setColor(0x88C1B1);
-		//;
-		//https://images-ext-2.discordapp.net/external/jOe59kBVkkwEGSxVxvdc-2KhOricRg8NPcyg7ZqfvII/https/cdn.discordapp.com/avatars/159410114665644032/3801e97d6e21e528df68d26192ccda49.png
-		embedBuilder.setFooter("Created by Jenspi", "https://images-ext-2.discordapp.net/external/jOe59kBVkkwEGSxVxvdc-2KhOricRg8NPcyg7ZqfvII/https/cdn.discordapp.com/avatars/159410114665644032/3801e97d6e21e528df68d26192ccda49.png");
+		String footerImg = "https://images-ext-2.discordapp.net/external/jOe59kBVkkwEGSxVxvdc-2KhOricRg8NPcyg7ZqfvII/https/cdn.discordapp.com/avatars/159410114665644032/3801e97d6e21e528df68d26192ccda49.png";
+		embedBuilder.setFooter("Created by Jenspi", footerImg);
 		channel.sendTyping().queue();
 		channel.sendMessageEmbeds(embedBuilder.build()).queue();
 		//clear embed to save memory
@@ -55,10 +35,9 @@ public class Help extends ModuleBase {
 	}*/
 	
 	//AESTHETICS FORMATTING HELPER METHODS:
-	//return italic code block
 	public String ic(String code)
 	{
-		return "*` "+code+"`*";
+		return "*`"+code+"`*";//return italic code block
 	}//end ic method
 	
 	//return command as italic code block followed by its description
